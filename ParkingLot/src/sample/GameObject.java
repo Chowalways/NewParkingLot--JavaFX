6cf9002a6e3144ac020042e9a503dfd775f99822
Gibson <gibsonkong6871@gmail.com>
2018-12-06 22:01:02 +0800

    add on Car Object and game scene

--- ParkingLot/src/sample/GameObject.java
+++ ParkingLot/src/sample/GameObject.java
@@ -1,11 +1,19 @@
 package sample;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
 
 public class GameObject {
     private Node view;
-    private Point2D velocity = new Point2D(0, 0);
+    private Point2D velocity = new Point2D(1, 0);
+    private boolean move = false;
+    private boolean reverse = false;
 
     private boolean alive = true;
 
@@ -29,9 +37,45 @@ public class GameObject {
         this.velocity = velocity;
     }
 
+    public void toggleStop() {
+        move = !move;
+    }
+
+    public void toggleReverse() {
+        reverse = !reverse;
+    }
+
     public void update() {
-        view .setTranslateX(view.getTranslateX() + velocity.getX());
-        view .setTranslateY(view.getTranslateY() + velocity.getY());
+        double newX = view.getTranslateX(),
+                newY = view.getTranslateY();
+
+        if(reverse){
+            newX -= velocity.getX();
+            newY -= velocity.getY();
+        } else {
+            newX += velocity.getX();
+            newY += velocity.getY();
+        }
+
+        if(move) {
+            if(checkX(newX))
+                view.setTranslateX(newX);
+            if(checkY(newY))
+                view.setTranslateY(newY);
+        }
+    }
+
+    private boolean checkX(double x) {
+        Bounds parentBounds = view.getParent().getLayoutBounds();
+        return parentBounds.getMaxX() - view.getLayoutBounds().getWidth() > x &&
+                parentBounds.getMinX() < x;
+    }
+
+    private boolean checkY(double y) {
+        Bounds parentBounds = view.getParent().getLayoutBounds();
+
+        return parentBounds.getMaxY() - view.getLayoutBounds().getHeight() >= y &&
+                parentBounds.getMinY() <= y;
     }
 
     public void setAlive(boolean alive) {
@@ -51,11 +95,28 @@ public class GameObject {
     }
 
     public void rotateRight() {
-        view.setRotate(view.getRotate() + 5);
+        view.setRotate((view.getRotate() + 5) % 360);
+        setVelocity(new Point2D(Math.cos(Math.toRadians(getRotate())), Math.sin(Math.toRadians(getRotate()))));
+        System.out.println(getRotate());
+        System.out.println(getVelocity());
     }
 
     public void rotateLeft() {
-        view.setRotate(view.getRotate() - 5);
+        view.setRotate((view.getRotate() - 5) % 360);
+        setVelocity(new Point2D(Math.cos(Math.toRadians(getRotate())), Math.sin(Math.toRadians(getRotate()))));
+        System.out.println(getRotate());
+    }
+
+    public final <T extends Event> void addEventFilter(
+            final EventType<T> eventType,
+            final EventHandler<? super T> eventFilter) {
+        view.addEventFilter(eventType, eventFilter);
+    }
+
+    public final <T extends Event> void removeEventFilter(
+            final EventType<T> eventType,
+            final EventHandler<? super T> eventFilter) {
+        view.removeEventFilter(eventType, eventFilter);
     }
 
     public boolean isColliding(GameObject other) {
--- ParkingLot/src/sample/GameScene.java
+++ ParkingLot/src/sample/GameScene.java
@@ -1,23 +1,140 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameScene extends Application {
 
-    List<GameObject> car = new ArrayList<>();
+    List<GameObject> cars = new ArrayList<>();
+    GameObject selectedObject = null;
 
+    @FXML
+    private Pane pane;
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+        // load fxml Scene from url
+        Parent root = FXMLLoader.load(getClass().getResource("gameScene.fxml"));
+        primaryStage.setTitle("Check In System");
+        Scene scene = new Scene(root);
 
+        // Update scene timer
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                onUpdate();
+            }
+        };
+
+        timer.start();
+
+        primaryStage.setScene(scene);
+        setControl(primaryStage);
+
+        // find pane from root
+        pane = (Pane) root.lookup("#pane");
+
+        // show the stage
+        primaryStage.show();
+
+        // create car if there is not exists (testing use)
+        if(cars.size() == 0) {
+            Car car = Car.createCar();
+
+            addCar(car, 300, 300);
+            car = Car.createCar();
+            addCar(car, 100, 200);
+        }
+    }
+
+    public void setControl(Stage stage) {
+        stage.getScene().setOnKeyPressed(e -> {
+            // ensure there has one car (Testing use)
+            if(selectedObject == null) {
+                return;
+            }
+
+            if(e.getCode() == KeyCode.DOWN) {
+                selectedObject.toggleReverse();
+            }
+
+            if(e.getCode() == KeyCode.UP) {
+                selectedObject.toggleStop();
+            }
+
+            if(e.getCode() == KeyCode.LEFT) {
+                selectedObject.rotateLeft();
+            }
+
+            if(e.getCode() == KeyCode.RIGHT) {
+                selectedObject.rotateRight();
+            }
+        });
+    }
+
+    private void addCar(GameObject object, double x, double y) {
+        object.addEventFilter(MouseEvent.MOUSE_CLICKED,
+                e -> {
+                    System.out.println("Click");
+                    selectedObject = object;
+//                    System.out.println(object instanceof Car);
+                });
+        cars.add(object);
+        addGameObject(object, x, y);
+    }
+
+    private void addGameObject(GameObject object, double x, double y) {
+        object.getView().setTranslateX(x);
+        object.getView().setTranslateY(y);
+        pane.getChildren().add(object.getView());
+
+    }
+
+    private void onUpdate() {
+        cars.forEach(GameObject::update);
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+
+    static class Car extends GameObject {
+
+
+        public Car(ImageView image) {
+            super(image);
+        }
+
+        public static Car createCar() {
+            ImageView imageView = new ImageView();
+            Image image = new Image("res/images/car.png");
+            imageView.setImage(image);
+            imageView.setFitWidth(25);
+            imageView.setFitHeight(25);
+
+            return new Car(imageView);
+        }
+
     }
 
 }
